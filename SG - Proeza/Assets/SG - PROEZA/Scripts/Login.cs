07e2c3330a1293f1fa635ad79139fa3883ecde06
Euclides <eneye280@gmail.com>
2018-04-26 09:05:45 -0500

    Update Animals

--- SG - Proeza/Assets/SG - PROEZA/Scripts/Login.cs
+++ SG - Proeza/Assets/SG - PROEZA/Scripts/Login.cs
@@ -9,14 +9,20 @@ public class Login : MonoBehaviour
 	[SerializeField] private InputField inputField;
 	[SerializeField] public Text textNombre;
 	[SerializeField] public Font fuente;
+	[SerializeField] private Camera cameraMain;
 	[SerializeField] internal GameObject[] enableDisable;
 
    	[HideInInspector] private string nombreInicial = "Ingrese su Nombre";
 
 	private void Awake()
 	{
+
+#region Singleton
+
 		if(login == null) login = this;
 		else Destroy(this);
+		
+#endregion
 
 		enableDisable[0].SetActive(false);
 		enableDisable[1].SetActive(false);
@@ -25,22 +31,32 @@ public class Login : MonoBehaviour
 		inputField.placeholder.GetComponent<Text>().text = nombreInicial.ToString();
 		inputField.placeholder.GetComponent<Text>().font = fuente;
 		inputField.placeholder.GetComponent<Text>().color = Color.black;
+
+#region Camera
+		cameraMain.clearFlags = CameraClearFlags.SolidColor;
+		cameraMain.backgroundColor = Color.white;
+		cameraMain.renderingPath = RenderingPath.DeferredShading;
+		cameraMain.farClipPlane = 0.31f;
+		cameraMain.allowMSAA = false;
+#endregion
+
 	}
 
-	/// <summary>
-	/// Update is called every frame, if the MonoBehaviour is enabled.
-	/// </summary>
-	void Update()
+	private void Update()
 	{
 		if(enableDisable[1].activeSelf)
 		{
 			
 		}
 	}
+
+#region Boton Inicio
 	public void Play () 
 	{
 		enableDisable[0].SetActive(true);
 		enableDisable[1].SetActive(true);
 		enableDisable[2].SetActive(false);
 	}
+#endregion
+
 }
